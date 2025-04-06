466a0427f7774f4bea2e926a7db864b3033a0118
fi1ss <akovlevk647@gmail.com>
2025-04-06 10:04:38 +0500

    da

--- frontend/src/Bar/Footer.jsx
+++ frontend/src/Bar/Footer.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './footer.css';
 
 function Footer() {
- 
   return (
-    <>
-      
-    </>
-  )
+    <footer className="footer">
+      <div className="footer-links">
+        <Link to="/about">О нас</Link>
+        <Link to="/terms">Пользовательское соглашение</Link>
+        <Link to="/privacy">Политика конфиденциальности</Link>
+        <Link to="/cookies">Политика использования cookies</Link>
+      </div>
+      <p>© {new Date().getFullYear()} Агентство инноваций</p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
