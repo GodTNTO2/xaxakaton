de716dd44e3cd7389163e770b5f65b232ad1de4e
GodTNTO2 <gleb.danilevich.2006@mail.ru>
2025-04-06 13:19:02 +0500

    a

--- frontend/src/Componets/Card.jsx
+++ frontend/src/Componets/Card.jsx
@@ -13,7 +13,7 @@ function Card({arrayData, cardName, cardText}) {
   return (
     <>
       <div className="cards-page fade-in">
-            <div onClick={pushOnClik} className="cards-grid fade-in">
+            <div onClick={pushOnClik} className="cards-grid ">
                 <div key={arrayData.id} className="cards-tile fade-in">
                   <img
                     src={arrayData.image || defImage}
